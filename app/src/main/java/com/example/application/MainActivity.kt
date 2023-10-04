857e7d47ddfaa0fa7304f6b3be178d8f7a0e9294
Vahe Hovsepyan <vahehovsepyan3@gmail.com>
2023-10-04 15:19:19 +0400

    Commit

--- app/src/main/java/com/example/application/MainActivity.kt
+++ app/src/main/java/com/example/application/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.application.CreateEvent.*
+import com.example.application.Downloads.DownloadsFragment
 import com.example.application.databinding.ActivityMainBinding
 import com.example.application.LoginAndRegister.interfaces.Interfaces
 
@@ -15,7 +16,7 @@ class MainActivity : AppCompatActivity(), Interfaces.CreateFragment {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        createFragment.createFragment(MyEventsFragment())
+        createFragment.createFragment(DownloadsFragment())
     }
 
     override fun createFragment(fragment: Fragment) {
